009a2b925212d62a504a273a62930d5b25d4d27c
edenfalaka <edenfalaka@gmail.com>
2024-09-06 00:01:35 +0300

    Deploy

---
+++ edenTools.tab/Dev.panel/extra.pulldown/helloWorld x.pushbutton/script.py
@@ -0,0 +1,6 @@
+__title__ = "Hello BIM World!"
+__author__ = "Eden Falaka"
+__doc__ = """This is Hello World Button. Click on it to see what happens"""
+
+if __name__ == '__main__':
+    print("Hello BIM World!")
