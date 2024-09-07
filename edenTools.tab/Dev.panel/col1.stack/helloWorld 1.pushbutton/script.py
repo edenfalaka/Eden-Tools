f24c0e3943435ec1e5dcf9ed45bafd882a4f4b25
edenfalaka <edenfalaka@gmail.com>
2024-09-08 00:28:40 +0300

    Add Export location

--- edenTools.tab/Dev.panel/col1.stack/helloWorld 1.pushbutton/script.py
+++
@@ -1,6 +0,0 @@
-__title__ = "Hello BIM World 1!"
-__author__ = "Eden Falaka"
-__doc__ = """This is Hello World Button. Click on it to see what happens"""
-
-if __name__ == '__main__':
-    print("Hello BIM World! 1")
--- lib/Snippets/_selection.py
+++ lib/Snippets/_selection.py
@@ -4,13 +4,13 @@
 from Autodesk.Revit.DB import *
 
 # VARIABLES
-uidoc = __revit__.ActiveUIDocument
-doc = __revit__.ActiveUIDocument.Document
+uidoc   = __revit__.ActiveUIDocument
+doc     = __revit__.ActiveUIDocument.Document
 
 # FUNCTIONS
 
-def get_selected_elements(uidoc):
-
+# Get Selected Elements
+def get_selected_elements(doc):
     """This function will return elements that are currently selected in Revit UI
     :params uidoc: uidoc where elements are selected
     return: list of selected elements
@@ -18,8 +18,52 @@ def get_selected_elements(uidoc):
 
     selected_elements = []
 
-    for elem_id  in uidoc.Selection.GetElementIds():
+    for elem_id in uidoc.Selection.GetElementIds():
         elem = uidoc.Document.GetElement(elem_id)
         selected_elements.append(elem)
 
     return selected_elements
+
+
+# Get Linked Models
+def get_linked_docs(doc):
+    """This function will return linked models, attached to our current Revit file
+    :params doc: Current Document
+    return: list of linked models in our current document
+    """
+
+    linked_docs = [
+        link_instance.GetLinkDocument()
+        for link_instance in FilteredElementCollector(doc).OfClass(RevitLinkInstance)
+        if link_instance.GetLinkDocument()
+    ]
+    return linked_docs
+
+
+# Get Project Basepoint Function
+def get_base_point(linked_docs, pbp=False, sp=False):
+    """This function will return which point to export
+    :params pbp: bool param insert true/false whether you want Project basepoint to export
+            sp: bool param insert true/false whether you want Survey point to export
+    return: List of selected points"""
+
+    # define empty values for pbp and sp
+    project_base_point = None
+    survey_point = None
+
+    base_point = FilteredElementCollector(linked_docs).OfClass(BasePoint).WhereElementIsNotElementType().ToElements()
+    for bp in base_point:
+        if not bp.IsShared:
+            project_base_point = bp
+        else:
+            survey_point = bp
+
+    # Return Options
+    if pbp and sp:
+        return project_base_point, survey_point
+    elif pbp:
+        return project_base_point
+    elif sp:
+        return survey_point
+    else:
+        return None
